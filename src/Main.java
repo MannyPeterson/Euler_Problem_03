56123405c1254dbff7270d8e78634702988d0595
Manny Peterson <me@mannypeterson.com>
2019-04-14 12:26:42 -0500

    update

--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,23 @@
  */
 
 public class Main {
-
-	public static void main(String[] args) {
-
+	public static boolean isPrime(long n) {
+		if (n <= 1)
+			return false;
+		if (n > 2 && (n & 1) == 0)
+			return false;
+		for (long i = 3; i <= Math.sqrt(n); i += 2)
+			if (n % i == 0)
+				return false;
+		return true;
 	}
 
+	public static void main(String[] args) {
+		long num = 600851475143L, largest = 0;
+		for (long i = 1; i < num; i++)
+			if (num % i == 0)
+				if (Main.isPrime(i))
+					largest = i;
+		System.out.format("ANSWER = %d\n", largest);
+	}
 }
